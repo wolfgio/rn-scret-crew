2e18271bb86100291e82f6ff844b93650075965c
Giovanny Piovesan <giovannywrp@hotmail.com>
2017-11-04 17:48:47 -0200

    fixed lint errors on App.js

--- App.js
+++ App.js
@@ -24,21 +24,19 @@ class App extends React.Component {
     isLoadingComplete: false,
   };
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync([
-        // This is the font that we are using for our tab bar
-        Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
-      ]),
-    ]);
-  };
+  _loadResourcesAsync = async () => Promise.all([
+    Asset.loadAsync([
+      require('./assets/images/robot-dev.png'),
+      require('./assets/images/robot-prod.png'),
+    ]),
+    Font.loadAsync([
+      // This is the font that we are using for our tab bar
+      Ionicons.font,
+      // We include SpaceMono because we use it in HomeScreen.js. Feel free
+      // to remove this if you are not using it in your app
+      { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+    ]),
+  ]);
 
   _handleLoadingError = (error) => {
     // In this case, you might want to report the error to your error
